d75bd4cebfd2209607798fd9f0bc231109af5341
Irnehs <henritschulz@gmail.com>
2023-04-13 14:15:57 -0700

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
+  ArrayList<String> str = new ArrayList<String>(10);
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
@@ -16,9 +19,20 @@ class Handler implements URLHandler {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("str")) {
+                    str.add(parameters[1]);
+                    return String.format("Added %s to list of Strings!", parameters[1]);
+                }
+            }
+            else if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("name")) {
+                    if(str.contains(parameters[1])) {
+                        return String.formnaat("%s is in the list of strings!", parameters[1]);
+                    }
+                    else {
+                        return String.format("%s is NOT in the list of strings!", parameters[1]);
+                    }
                 }
             }
             return "404 Not Found!";
